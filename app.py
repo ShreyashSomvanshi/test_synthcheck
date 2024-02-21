7c664aa881aab5d5a5b9c06eba067cc8d9a772a0
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2024-02-21 22:35:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.write("Upload an image to check whether it is a fake or real image.")
 
 file_uploaded = st.file_uploader("Choose the Image File", type=["jpg", "png", "jpeg"])
 
-if st.button('Check', use_container_width=True) #use_container_width=True
+if st.button('Check', use_container_width=True):
     if file_uploaded is not None:
         res = classify_image(file_uploaded)
         c1, buff, c2 = st.columns([2, 0.5, 2])
